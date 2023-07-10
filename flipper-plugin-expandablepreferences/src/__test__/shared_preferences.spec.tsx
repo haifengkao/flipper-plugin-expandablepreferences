0b00647e066c9428f42ff299dc3282e1293b78fa
Hai Feng Kao <>
2023-07-10 21:21:31 +0800

    copy the shared preference viewer from Facebook/Flipper

---
+++ flipper-plugin-expandablepreferences/src/__test__/shared_preferences.spec.tsx
@@ -0,0 +1,152 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+import {TestUtils, sleep} from 'flipper-plugin';
+import * as plugin from '..';
+
+// this testing is inspired by Flipper sample app
+test('general plugin logic testing', async () => {
+  const {instance, onSend, connect, sendEvent} = TestUtils.startPlugin(plugin, {
+    startUnactivated: true,
+  });
+  onSend.mockImplementation(async (method, params) => {
+    switch (method) {
+      case 'getAllSharedPreferences':
+        return {
+          sample: {Hello: 'world'},
+          other_sample: {SomeKey: 1337},
+        };
+      case 'setSharedPreference': {
+        const p = params as plugin.SetSharedPreferenceParams;
+        return {[p.preferenceName]: p.preferenceValue};
+      }
+      case 'deleteSharedPreference': {
+        return {};
+      }
+    }
+  });
+
+  // Retrieve some data when connect
+  connect();
+  await sleep(1000);
+  expect(onSend).toBeCalledWith('getAllSharedPreferences', {});
+  expect(instance.sharedPreferences.get()).toMatchInlineSnapshot(`
+    {
+      "other_sample": {
+        "changesList": [],
+        "preferences": {
+          "SomeKey": 1337,
+        },
+      },
+      "sample": {
+        "changesList": [],
+        "preferences": {
+          "Hello": "world",
+        },
+      },
+    }
+  `);
+  expect(instance.selectedPreferences.get()).toEqual('sample');
+
+  instance.setSelectedPreferences('other_sample');
+  expect(instance.selectedPreferences.get()).toEqual('other_sample');
+
+  // test changing preference
+  const changedPref = {
+    sharedPreferencesName: 'other_sample',
+    preferenceName: 'SomeKey',
+    preferenceValue: 5555,
+  };
+  await instance.setSharedPreference(changedPref);
+  // this is sent from client after successful update
+  sendEvent('sharedPreferencesChange', {
+    deleted: false,
+    name: 'SomeKey',
+    preferences: 'sample',
+    time: 1,
+    value: 5555,
+  });
+  expect(onSend).toBeCalledWith('setSharedPreference', changedPref);
+  expect(instance.sharedPreferences.get().sample.preferences.SomeKey).toEqual(
+    5555,
+  );
+  expect(instance.sharedPreferences.get()).toMatchInlineSnapshot(`
+    {
+      "other_sample": {
+        "changesList": [],
+        "preferences": {
+          "SomeKey": 5555,
+        },
+      },
+      "sample": {
+        "changesList": [
+          {
+            "deleted": false,
+            "name": "SomeKey",
+            "preferences": "sample",
+            "time": 1,
+            "value": 5555,
+          },
+        ],
+        "preferences": {
+          "Hello": "world",
+          "SomeKey": 5555,
+        },
+      },
+    }
+  `);
+
+  // test deleting preference
+  const deletedPref = {
+    sharedPreferencesName: 'other_sample',
+    preferenceName: 'SomeKey',
+  };
+  await instance.deleteSharedPreference(deletedPref);
+  // this is sent from client after successful update
+  sendEvent('sharedPreferencesChange', {
+    deleted: true,
+    name: 'SomeKey',
+    preferences: 'sample',
+    time: 2,
+  });
+  expect(onSend).toBeCalledWith('deleteSharedPreference', deletedPref);
+  expect(
+    instance.sharedPreferences.get().sample.preferences.SomeKey,
+  ).toBeUndefined();
+  expect(instance.sharedPreferences.get()).toMatchInlineSnapshot(`
+    {
+      "other_sample": {
+        "changesList": [],
+        "preferences": {},
+      },
+      "sample": {
+        "changesList": [
+          {
+            "deleted": true,
+            "name": "SomeKey",
+            "preferences": "sample",
+            "time": 2,
+          },
+          {
+            "deleted": false,
+            "name": "SomeKey",
+            "preferences": "sample",
+            "time": 1,
+            "value": 5555,
+          },
+        ],
+        "preferences": {
+          "Hello": "world",
+        },
+      },
+    }
+  `);
+});
+
+// TODO: Add unit test for UI
--- flipper-plugin-expandablepreferences/src/index.tsx
+++ flipper-plugin-expandablepreferences/src/index.tsx
@@ -1,50 +1,298 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+import {
+  ManagedTable,
+  Text,
+  Heading,
+  FlexColumn,
+  colors,
+  FlexRow,
+  ManagedDataInspector,
+  styled,
+  Select,
+} from 'flipper';
+import {
+  PluginClient,
+  createState,
+  usePlugin,
+  useValue,
+  getFlipperLib,
+} from 'flipper-plugin';
+import {clone} from 'lodash';
+
 import React from 'react';
-import {PluginClient, usePlugin, createState, useValue, Layout} from 'flipper-plugin';
-
-type Data = {
-  id: string;
-  message?: string;
+import {Button, notification} from 'antd';
+type SharedPreferencesChangeEvent = {
+  preferences: string;
+  name: string;
+  time: number;
+  deleted: boolean;
+  value?: any;
+};
+type SharedPreferences = Record<string, any>;
+type SharedPreferencesEntry = {
+  preferences: SharedPreferences;
+  changesList: Array<SharedPreferencesChangeEvent>;
 };
 
-type Events = {
-  newData: Data;
+export type SetSharedPreferenceParams = {
+  sharedPreferencesName: string;
+  preferenceName: string;
+  preferenceValue: any;
+};
+type DeleteSharedPreferenceParams = {
+  sharedPreferencesName: string;
+  preferenceName: string;
 };
 
-// Read more: https://fbflipper.com/docs/tutorial/js-custom#creating-a-first-plugin
-// API: https://fbflipper.com/docs/extending/flipper-plugin#pluginclient
-export function plugin(client: PluginClient<Events, {}>) {
-  const data = createState<Record<string, Data>>({}, {persist: 'data'});
+type Events = {sharedPreferencesChange: SharedPreferencesChangeEvent};
+type Methods = {
+  getAllSharedPreferences: (params: {}) => Promise<
+    Record<string, SharedPreferences>
+  >;
+  setSharedPreference: (
+    params: SetSharedPreferenceParams,
+  ) => Promise<SharedPreferences>;
+  deleteSharedPreference: (
+    params: DeleteSharedPreferenceParams,
+  ) => Promise<SharedPreferences>;
+};
 
-  client.onMessage('newData', (newData) => {
-    data.update((draft) => {
-      draft[newData.id] = newData;
+export function plugin(client: PluginClient<Events, Methods>) {
+  const selectedPreferences = createState<string | null>(null, {
+    persist: 'selectedPreferences',
+  });
+  const setSelectedPreferences = (value: string) =>
+    selectedPreferences.set(value);
+  const sharedPreferences = createState<Record<string, SharedPreferencesEntry>>(
+    {},
+    {persist: 'sharedPreferences'},
+  );
+
+  function updateSharedPreferences(update: {name: string; preferences: any}) {
+    if (selectedPreferences.get() == null) {
+      selectedPreferences.set(update.name);
+    }
+    sharedPreferences.update((draft) => {
+      const entry = draft[update.name] || {changesList: []};
+      entry.preferences = update.preferences;
+      draft[update.name] = entry;
     });
+  }
+
+  async function setSharedPreference(params: SetSharedPreferenceParams) {
+    const results = await client.send('setSharedPreference', params);
+    updateSharedPreferences({
+      name: params.sharedPreferencesName,
+      preferences: results,
+    });
+  }
+  async function deleteSharedPreference(params: DeleteSharedPreferenceParams) {
+    const results = await client.send('deleteSharedPreference', params);
+    updateSharedPreferences({
+      name: params.sharedPreferencesName,
+      preferences: results,
+    });
+  }
+
+  async function saveToFile() {
+    if (selectedPreferences.get() != null) {
+      try {
+        const name = selectedPreferences.get() as string;
+        await getFlipperLib().exportFile(
+          JSON.stringify(sharedPreferences.get()[name]),
+          {
+            defaultPath: name,
+          },
+        );
+      } catch (e) {
+        notification.error({
+          message: 'Save failed',
+          description: `Could not save shared preferences to file`,
+          duration: 15,
+        });
+      }
+    }
+  }
+  async function loadFromFile() {
+    const file = await getFlipperLib().importFile();
+    if (file?.path != undefined) {
+      const data = await getFlipperLib().remoteServerContext.fs.readFile(
+        file.path,
+        {encoding: 'utf-8'},
+      );
+      const preferences = JSON.parse(data) as SharedPreferencesEntry;
+      const name = selectedPreferences.get();
+      if (name != null) {
+        updateSharedPreferences({
+          name: name,
+          preferences: preferences.preferences,
+        });
+
+        for (const key in preferences.preferences) {
+          await client.send('setSharedPreference', {
+            sharedPreferencesName: name,
+            preferenceName: key,
+            preferenceValue: preferences.preferences[key],
+          });
+        }
+      }
+    }
+  }
+
+  client.onMessage('sharedPreferencesChange', (change) =>
+    sharedPreferences.update((draft) => {
+      const entry = draft[change.preferences];
+      if (entry == null) {
+        return;
+      }
+      if (change.deleted) {
+        delete entry.preferences[change.name];
+      } else {
+        entry.preferences[change.name] = change.value;
+      }
+      entry.changesList.unshift(change);
+      draft[change.preferences] = entry;
+    }),
+  );
+  client.onConnect(async () => {
+    const results = await client.send('getAllSharedPreferences', {});
+    Object.entries(results).forEach(([name, prefs]) =>
+      updateSharedPreferences({name: name, preferences: prefs}),
+    );
   });
 
-  client.addMenuEntry({
-    action: 'clear',
-    handler: async () => {
-      data.set({});
-    },
-    accelerator: 'ctrl+l',
-  });
-
-  return {data};
+  return {
+    selectedPreferences,
+    sharedPreferences,
+    setSelectedPreferences,
+    setSharedPreference,
+    deleteSharedPreference,
+    saveToFile,
+    loadFromFile,
+  };
 }
 
-// Read more: https://fbflipper.com/docs/tutorial/js-custom#building-a-user-interface-for-the-plugin
-// API: https://fbflipper.com/docs/extending/flipper-plugin#react-hooks
+const CHANGELOG_COLUMNS = {
+  event: {value: 'Event'},
+  name: {value: 'Name'},
+  value: {value: 'Value'},
+};
+const CHANGELOG_COLUMN_SIZES = {
+  event: '30%',
+  name: '30%',
+  value: '30%',
+};
+
+const UPDATED_LABEL = <Text color={colors.lime}>Updated</Text>;
+const DELETED_LABEL = <Text color={colors.cherry}>Deleted</Text>;
+
+const InspectorColumn = styled(FlexColumn)({flexGrow: 0.2});
+const ChangelogColumn = styled(FlexColumn)({
+  flexGrow: 0.8,
+  paddingLeft: '16px',
+});
+const RootColumn = styled(FlexColumn)({
+  paddingLeft: '16px',
+  paddingRight: '16px',
+  paddingTop: '16px',
+});
+
 export function Component() {
   const instance = usePlugin(plugin);
-  const data = useValue(instance.data);
+  const selectedPreferences = useValue(instance.selectedPreferences);
+  const sharedPreferences = useValue(instance.sharedPreferences);
+
+  if (selectedPreferences == null) {
+    return null;
+  }
+  const entry = sharedPreferences[selectedPreferences];
+  if (entry == null) {
+    return null;
+  }
 
   return (
-    <Layout.ScrollContainer>
-      {Object.entries(data).map(([id, d]) => (
-        <pre key={id} data-testid={id}>
-          {JSON.stringify(d)}
-        </pre>
-      ))}
-    </Layout.ScrollContainer>
+    <RootColumn grow>
+      <Heading>
+        <span style={{marginRight: '16px'}}>Preference File</span>
+        <Select
+          options={Object.keys(sharedPreferences)
+            .sort((a, b) => (a.toLowerCase() > b.toLowerCase() ? 1 : -1))
+            .reduce((obj, item) => {
+              obj[item] = item;
+              return obj;
+            }, {} as Record<string, string>)}
+          selected={selectedPreferences}
+          onChange={instance.setSelectedPreferences}
+        />
+        <span style={{marginLeft: '16px', marginRight: '16px'}}>Options</span>
+        <Button size="small" onClick={() => instance.saveToFile()}>
+          Save
+        </Button>
+        <Button
+          style={{marginLeft: '8px'}}
+          size="small"
+          onClick={() => instance.loadFromFile()}>
+          Load
+        </Button>
+      </Heading>
+      <FlexRow grow scrollable style={{overflowX: 'hidden'}}>
+        <InspectorColumn>
+          <Heading>Inspector</Heading>
+          <ManagedDataInspector
+            data={entry.preferences}
+            setValue={async (path: Array<string>, value: any) => {
+              if (entry == null) {
+                return;
+              }
+              const values = entry.preferences;
+              let newValue = value;
+              if (path.length === 2 && values) {
+                newValue = clone(values[path[0]]);
+                newValue[path[1]] = value;
+              }
+              await instance.setSharedPreference({
+                sharedPreferencesName: selectedPreferences,
+                preferenceName: path[0],
+                preferenceValue: newValue,
+              });
+            }}
+            onDelete={async (path: Array<string>) =>
+              await instance.deleteSharedPreference({
+                sharedPreferencesName: selectedPreferences,
+                preferenceName: path[0],
+              })
+            }
+          />
+        </InspectorColumn>
+        <ChangelogColumn>
+          <Heading>Changelog</Heading>
+          <ManagedTable
+            columnSizes={CHANGELOG_COLUMN_SIZES}
+            columns={CHANGELOG_COLUMNS}
+            rowLineHeight={26}
+            rows={entry.changesList.map((element, index) => {
+              return {
+                columns: {
+                  event: {
+                    value: element.deleted ? DELETED_LABEL : UPDATED_LABEL,
+                  },
+                  name: {value: element.name},
+                  value: {value: String(element.value)},
+                },
+                key: String(index),
+              };
+            })}
+          />
+        </ChangelogColumn>
+      </FlexRow>
+    </RootColumn>
   );
 }
